16a0d1d4d25b30c096ca0bc27b7a3aaf9f9ae6f9
chikonwong <chikonwong@gmail.com>
2021-02-18 00:15:08 +0800

    init

--- app/src/main/java/com/example/qrcodereader_android/MainActivity.java
+++ app/src/main/java/com/example/qrcodereader_android/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.qrcodereader_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.SettingsSlicesContract;
 import android.view.View;
+import android.widget.Toast;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
 
 public class MainActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("hi"); // set inside app title
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
     }
-    public void launchSettings (View v){
-        // launch a new acrivity
 
-        Intent i = new Intent(this, SettingsActivity.class);
-        startActivity(i);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
     }
 }
